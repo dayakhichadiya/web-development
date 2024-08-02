06c25611a087769d4f3f7c76c6ad5bdcf07b15a4
vijay vekariya <vijayvekariya007@gmail.com>
2024-08-02 18:03:21 +0530

    2-8-2024

--- 7.Tailwind-css/tailwind.config.js
+++ 7.Tailwind-css/tailwind.config.js
@@ -5,4 +5,19 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+
+  screens: {
+    sm: '600px',
+    md: '768px',
+    lg: '1199px',
+    xl: '1440px',
+  },
+  borderRadius: {
+    '4xl': '2rem',
+    '5xl': '2.5rem',
+    '6xl': '3rem',
+    '7xl': '3.5rem',
+    '8xl': '4rem',
+    '9xl': '4.5rem',
+}
 }
