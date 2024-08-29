a99fe2dd8d22db6a8acb81c7de544c0dcd6ce5dc
vijay vekariya <krishtopiwala1724@gmail.com>
2024-08-29 18:16:40 +0530

    29-8-24 js

---
+++ 9.JavaScript/lecture2-variables/java.js
@@ -0,0 +1,9 @@
+document.getElementById("demo").innerHTML="DOM method";
+document.getElementById("demo").innerHTML="changed paragraph.";
+
+var a=10;
+console.log('a');
+
+var str='skill'
+console.log('str');
+
