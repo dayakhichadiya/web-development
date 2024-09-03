9fbb1276b5eb46740a27c9150f86e4593e0e96c7
vijay vekariya <krishtopiwala1724@gmail.com>
2024-09-03 18:07:09 +0530

    03-09-24

---
+++ 9.JavaScript/lecture5-operators/java.js
@@ -0,0 +1,13 @@
+// operators in javascript
+
+// 1.Arithmetic operator
+// 2.Assignment operator
+// 3.Comparison operator
+// 4. Conditional operator
+// 5. Logical operator
+// 6. unary operator
+// 7. binary operator
+// 8.ternary operatoe
+// 9.string operator
+
+
