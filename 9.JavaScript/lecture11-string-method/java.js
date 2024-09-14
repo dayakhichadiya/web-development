05f87d10d7334243b9603aec558661df479e8617
vijay vekariya <krishtopiwala1724@gmail.com>
2024-09-14 18:23:27 +0530

    14-09-24

--- 9.JavaScript/lecture11-string-method/java.js
+++ 9.JavaScript/lecture11-string-method/java.js
@@ -56,8 +56,69 @@
 
 {
     let str1 = "Hello"
-    let str2 = "Javascript"
+    let str2 = "  Javascript"
 
     console.log(str1.concat(str2));
-    console.log(str2.concat(str1));
+}
+
+// String StartWith() / EndWith() Method 
+
+// syntax :
+// startsWith(searchString)
+// startsWith(searchString, position)
+
+// endsWith(searchString)
+// endsWith(searchString, endPosition)
+
+// NOTE : case-sensitive
+
+{
+    let str = "this is string method in JS"
+
+    console.log(str.startsWith('t')); //true
+    console.log(str.startsWith('T')); //false
+    console.log(str.startsWith('i',5)); //true
+    console.log(str.endsWith('s')); //false
+    console.log(str.endsWith('S'));// true
+}
+
+// String includes() Method 
+// Syntax:
+// includes(searchString)
+// includes(searchString, position)
+//NOTE : case-sensitive 
+
+{
+    let str = "this is include method in javascript!!"
+
+    console.log(str.includes('i')); //true
+    console.log(str.includes('in',2)); //true
+    console.log(str.includes('is',19 )); //false
+    console.log(str.includes('is',2)); //true
+    console.log(str.includes('i')); //true
+    console.log(str.includes('is',-20)); //empty bcz -20 means 0
+}
+
+
+// String indexOf() And lastIndexOf() Method 
+
+//Syntax:
+// indexOf(searchString)
+// indexOf(searchString, position)
+// case-sensitive 
+
+// lastIndexOf(searchString)
+// lastIndexOf(searchString, position)
+
+{
+    let str = "Cyprus, officially the Republic of Cyprus, is an island country in the eastern Sea."
+               123456789012345678901234567890123456789012345678901234567890123456789
+
+    console.log(str.indexOf('Sea')); //93
+    console.log(str.indexOf('the', 1)); //19
+    console.log(str.indexOf('sea')); //-1
+
+    console.log(str.lastIndexOf('the')) //67
+    console.log(str.lastIndexOf('of')) //67
+    console.log(str.lastIndexOf('an')) //67
 }
