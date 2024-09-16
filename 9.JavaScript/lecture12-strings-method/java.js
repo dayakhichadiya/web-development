7c5ec3c7d00c7f1d6ed092f46d73af436b82e6a0
vijay vekariya <krishtopiwala1724@gmail.com>
2024-09-16 18:12:46 +0530

    16-09-24

--- 9.JavaScript/lecture12-strings-method/java.js
+++ 9.JavaScript/lecture12-strings-method/java.js
@@ -59,5 +59,115 @@
     console.log(str.substring(12)); // ng method in javascript
     console.log(str.substring(-12)); // it give full string bcz negative value becms 0 
     console.log(str.slice(12, 24)); // ng method in
+    console.log(str.slice(12, -4)); // ng method in javasc
+    console.log(str.slice(-20, -28)); // empty
+    console.log(str.substring(-12, -24)); // empty
+
+    
+}
+
+//  String Search() Method 
+
+// Syntax:
+// search(regexp)
+{
+    let str = 'I think my car is beautiful than your Car!';
+    let regexp1 = /car/i;
+    let regexp2 = /Car/g;
+    
+    console.log(str.search(regexp1));//11
+    console.log(str.search(regexp2));//38
 
 }
+
+// String toLowercase() and toUppercase Method 
+// Syntax:
+
+// toUpperCase()
+// toLowerCase()
+
+{
+    let str = 'I think my car is beautiful than your Car!';
+    console.log(str.toLowerCase());
+    console.log(str.toUpperCase());
+}
+
+// String replace() and repalaceAll() Method 
+
+// syntax:
+
+// replace(pattern, replacement)
+// replaceAll(pattern, replacement)
+{
+    let str = 'I think my car is beautiful than your Car!';
+    let regexp1 = /car/g;
+    console.log(str.replace(regexp1,'CAR'));
+    console.log(str.replaceAll(regexp1,'CAr'));
+
+}
+
+// String match() and matchAll() Method
+
+// Syntax:
+
+// match(regexp)
+// matchAll(regexp)
+{
+    let str = 'I think my car is beautiful car than your Car!';
+    let regex1 = /car/g
+    console.log(str.match(regex1));
+    console.log(([...str.matchAll(regex1)]));
+
+    let array = [1,2,3,4,5,6,7]
+    let array1 = [...array]
+    let array2 = array
+
+    console.log(array);
+    console.log(array1);
+    console.log(array2);
+
+    // array[2] = 10
+
+    // console.log(array);
+    // console.log(array1);
+    // console.log(array2);
+    
+}
+// String split() Method 
+
+// split()
+
+{
+    let str = 'I think my car is beautiful car than your Car!';
+
+     let str1 = str.split('')
+     let str2 = str1.reverse()
+     let str3 = str2.join('')
+
+     console.log(str1);
+     console.log(str2);
+     console.log(str3);
+     
+}
+
+/* String repeat() Method */
+
+{
+     let str = "o";
+     console.log(str.repeat(10));
+}
+
+/* String toString and valueOf Method */
+
+{
+    let str = 10
+
+    console.log(str.toString());
+    console.log(str.valueOf());
+
+}
+
+
+
+
+
