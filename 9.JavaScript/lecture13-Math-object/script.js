882ec132db7068a2b066823e30e81d1a8db629ed
vijay vekariya <krishtopiwala1724@gmail.com>
2024-09-18 18:09:48 +0530

    18-09-24

---
+++ 9.JavaScript/lecture13-Math-object/script.js
@@ -0,0 +1,114 @@
+/* Javascript Math.Object Method */
+
+/* Static methods */
+
+// Math.abs()
+// Math.ceil()
+// Math.floor()
+// Math.max()
+// Math.min()
+// Math.pow()
+// Math.random()
+// Math.round()
+// Math.sin()
+// Math.sqrt()
+// Math.trunc()
+// Math.sign()
+// Math.cos()
+// Math.log()
+// Math.log10()
+// Math.log2()
+
+/* Static properties */
+
+// Math.E
+// Math.PI
+
+{
+    let math1 = Math.PI;
+    console.log(math1);
+}
+{
+    let math2 = Math.E;
+    console.log(math2);
+}
+
+//Math abs (absolute)
+{
+    let maths3 = Math.abs(29);
+    let maths4 = Math.abs(-29);
+    let maths5 = Math.abs(0);
+    let maths6 = Math.abs(-0);
+    console.log(maths3);
+    console.log(maths4);
+    console.log(maths5); 
+    console.log(maths6); 
+}
+
+// Math ceil 
+
+{
+    let math7 = Math.ceil(1.9);
+    let math8 = Math.ceil(-1.9);
+    let math9 = Math.ceil(1.2);
+    console.log(math7);
+    console.log(math8); //-1
+    console.log(math9)
+}
+
+// Math floor method
+
+{
+    let math10 = Math.floor(5.45);
+    let math11 = Math.floor(3.10)
+    console.log(math10);
+    console.log(math11);
+}
+
+// Math max 
+{
+    let math12 = Math.max(22, 18, 19, 90);
+    console.log(math12);
+}
+
+// Math min
+{
+    let math13 = Math.min(90, 58, 89, 33, 60);
+    console.log(math13);
+}
+
+// Math.pow()
+
+{
+    let math14 = Math.pow(4);
+    console.log(math14)
+}
+// Math.random()
+
+{
+    let math15 =  Math.random(23);
+    console.log(math15);
+}
+// Math.round()
+{
+    let math15 = Math.round(22)
+    console.log(math15);
+
+}
+// Math.sin()
+{
+    let math16 = Math.sin();
+    console.log(math16)
+}
+// Math.sqrt()
+{
+    let math17 = Math.sqrt(4);
+    console.log(math17);
+}
+
+// Math.trunc()
+// Math.sign()
+// Math.cos()
+// Math.log()
+// Math.log10()
+// Math.log2()
