3f0e60b84ba27193afee04d638b2448e03e35530
vijay vekariya <krishtopiwala1724@gmail.com>
2024-09-19 18:06:42 +0530

    19-09-24

--- 9.JavaScript/lecture13-Math-object/script.js
+++ 9.JavaScript/lecture13-Math-object/script.js
@@ -50,65 +50,97 @@
 {
     let math7 = Math.ceil(1.9);
     let math8 = Math.ceil(-1.9);
-    let math9 = Math.ceil(1.2);
-    console.log(math7);
-    console.log(math8); //-1
-    console.log(math9)
+    let math9 = Math.ceil(1.10);
+    console.log(math7); //2
+    console.log('ceil',math8); //-1
+    console.log(math9)//2
 }
 
 // Math floor method
 
 {
-    let math10 = Math.floor(5.45);
+    let math10 = Math.floor(5.90);
     let math11 = Math.floor(3.10)
-    console.log(math10);
-    console.log(math11);
+    console.log('floor value',math10); // 5
+    console.log(math11); // 3
 }
 
 // Math max 
 {
     let math12 = Math.max(22, 18, 19, 90);
-    console.log(math12);
+    console.log('maximum value is',math12); // 90
 }
 
 // Math min
 {
     let math13 = Math.min(90, 58, 89, 33, 60);
-    console.log(math13);
+    console.log('minimum value is',math13);//33
 }
 
 // Math.pow()
 
 {
-    let math14 = Math.pow(4);
+    let math14 = Math.pow(3,5); //3*3*3*3*3 =243
     console.log(math14)
 }
 // Math.random()
 
 {
     let math15 =  Math.random(23);
-    console.log(math15);
+    console.log('random number is',math15);
 }
 // Math.round()
 {
-    let math15 = Math.round(22)
-    console.log(math15);
+
+    let math16 = Math.round(Math.random()*10000)
+    console.log('round figure number is',math16);
 
 }
 // Math.sin()
 {
-    let math16 = Math.sin();
-    console.log(math16)
+    let math17 = Math.sin(30*3.14/180);
+    console.log('sin is',math17)
 }
 // Math.sqrt()
 {
-    let math17 = Math.sqrt(4);
-    console.log(math17);
+    let math18 = Math.sqrt(64);
+    console.log(math18);//8
 }
 
 // Math.trunc()
+{
+    let math19 = Math.trunc(10.10);
+    console.log(math19)
+}
 // Math.sign()
+
+{
+    let math20 = Math.sign(90);
+    let math21 = Math.sign(-56);
+    let math22 = Math.sign(0);
+
+    console.log(math20); // 1
+    console.log(math21); // -1
+    console.log(math22); // 0
+
+}
 // Math.cos()
+
+{
+    let math23 = Math.cos(60*3.14/180);
+    console.log(math23)
+
+}
 // Math.log()
 // Math.log10()
 // Math.log2()
+
+{
+    let math24 = Math.log(10);
+    let math25 = Math.log2(10);
+    let math26 = Math.log10(10);
+
+    console.log(math24);
+    console.log(math25);
+    console.log(math26);
+}
--- 9.JavaScript/lecuture14-NullNumberUndefined/script.js
+++ 9.JavaScript/lecuture14-NullNumberUndefined/script.js
@@ -1,1 +1,95 @@
+// NaN , null  , undefined 
+
+//{
+//   console.log(undefined == undefined);
+//   console.log(undefined == null);
+//   console.log(null == null);
+//   console.log(undefined == NaN);
+//   console.log(NaN == NaN);
+//   console.log(null == NaN);
+//   console.log(Infinity == undefined);
+//   console.log(Infinity == null);
+//   console.log(Infinity == NaN);
+//   console.log(Infinity == Infinity);
+//   console.log(-Infinity == Infinity);
+//}
+
+// Number Method
+
+// toString() 
+// toExponential()
+// toFixed()
+// toPrecision()
+// Valueof()
+
+//Converting Variable to Numbers
+// Number()
+// parseFloat() 
+// parseInt() 
+
+// Number Object Method
+// Number.isInteger() 
+// Number.isSafeInteger()
+// Number.parseFlaot()
+// Number.parseInt() 
+
+// to string()
+
+{
+    let num1 = 12;
+    console.log(num1);
+    console.log(typeof num1);
+    console.log(num1.toString());
+    console.log(typeof num1)
+
+}
+
+// toExponential()
+{
+    let num2 = 4.903202
+    console.log(num2.toExponential())
+    console.log(num2.toExponential(2))
+    console.log(num2.toExponential(3))
+}
+
+// to fixed()
+{
+    let num3 = 9.96701
+    console.log(num3.toFixed()); //10
+    console.log(num3.toFixed(2));//9.97
+    console.log(num3.toFixed(4));//9.9670
+}
+
+// toPrecision()
+
+{
+    let num4 = 9.9570;
+    console.log(num4.toPrecision()); //9.9670
+    console.log(num4.toPrecision(2));//10
+    console.log(num4.toPrecision(6));//9.95700
+}
+
+// Valueof()
+{
+    let num5 = 123
+    console.log(num5.valueOf())//123
+}
+
+
+// Number()
+{
+    let num6 = Number('10');
+    console.log(num6);
+    console.log(typeof num6)
+}
+
+// Number.isFinite(value)
+
+{
+    let num7 = 0/1;
+    console.log(num7);
+    let num8 = 1/0;
+    console.log(num8);
+
     
+}
