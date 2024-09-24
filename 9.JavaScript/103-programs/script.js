85d7cfa8a5c3936d1f16e5e08ae6119f4c4e0e53
dayakhichadiya <khichadiyadayam@gmail.com>
2024-09-24 11:34:19 +0530

    24-9-24

---
+++ 9.JavaScript/103-programs/script.js
@@ -0,0 +1,91 @@
+// 1) JavaScript Program To Print Hello World
+
+{
+    let text = 'hello world';
+    console.log(text)
+}
+
+
+// 2) JavaScript Program to Add Two Numbers
+
+{
+    let num1 = 30;
+    let num2 = 40;
+
+    let sum = num1 + num2;
+    console.log(sum);
+}
+
+// 3) JavaScript Program to Find the Square Root
+
+{
+    let num = 64;
+
+    let result = Math.sqrt(num);
+    console.log(result);
+}
+
+// 4) JavaScript Program to Calculate the Area of a Triangle
+
+{
+    let base = 5;
+    let height = 8;
+
+    let area = base * height / 2;
+    console.log('area o triangle is',area);
+}
+
+// 5) JavaScript Program to Swap Two Variables
+
+{
+    let a = 5;
+    let b = 10;
+
+    let swap ;
+
+    swap = a;
+    a = b;
+    b = swap;
+
+    console.log('a is',a);
+    console.log('b is',b);
+}
+
+// 6) JavaScript Program to Solve Quadratic Equation
+
+
+
+// 7) JavaScript Program to Convert Kilometres to Miles  39370
+
+{
+    let km = 5;
+    let fact = 0.621371;
+    
+    let miles = km * fact;
+    console.log(miles);
+}
+
+// 8) Javascript Program to Convert Celsius to Fahrenheit
+
+{
+    let celsius = 55;
+    let fahrenheit = (celsius * 1.8) + 32;
+
+    console.log(fahrenheit);
+}
+
+//9) Javascript Program to Generate a Random Number
+// 10)Javascript Program to Check if a number is Positive, Negative, or Zero
+// 11) Javascript Program to Check if a Number is Odd or Even
+// 12)JavaScript Program to Find the Largest Among Three Numbers
+// 13)JavaScript Program to Check Prime Number
+// 14)JavaScript Program to Print All Prime Numbers in an Interval
+// 15)JavaScript Program to Find the Factorial of a Number
+// 16)JavaScript Program to Display the Multiplication Table
+// 17)JavaScript Program to Print the Fibonacci Sequence
+// 18)JavaScript Program to Check Armstrong Number
+// 19)JavaScript Program to Find Armstrong Number in an Interval
+// 20)JavaScript Program to Make a Simple Calculator
+// 21)JavaScript Program to Find the Sum of Natural Numbers
+// 22)JavaScript Program to Check if the Numbers Have Same Last Digit
+// 23)JavaScript Program to Find HCF or GCD
