ce87f473e281f9d3aeb5b3ac0fc071dc7711f84f
vijay vekariya <krishtopiwala1724@gmail.com>
2024-10-18 18:08:54 +0530

    18-10-24

--- 10.JQuery/lecture2/script.js
+++ 10.JQuery/lecture2/script.js
@@ -29,13 +29,99 @@
 
 // jQuery Animate Effect
 
-$(function(){
-    $('#box').click(function(){
-        $(this).animate({
-            left: '500px',
-            height: '400px'
-        }, 2000)
-    })
 
-})
+// $(function(){
+//     $('#box').click(function(){
+//         $(this).animate({
+//             left: '500px',
+//             height: '400px'
+//         }, 200)
+//     })
 
+// })
+
+
+// jQuery Traversing
+{
+
+    // $(function(){
+    //     $('.p').click(function(){
+    //         $(this).parent().css('background', 'green')
+    //     })
+    // })
+
+    // $(function(){
+    //     $('.ul').click(function(){
+    //         $(this).parents().css('background', 'green')
+    //     })
+    // })
+
+    // $(function(){
+    //     $('.span-1').click(function(){
+    //         $(this).parentsUntil().css('background', 'green')
+    //     })
+    // })
+}
+
+
+// JQuery Get and Set Method
+
+/*
+
+html()
+text()
+val()
+
+*/
+
+{
+    // $(function(){
+    //     $('#btn').click(function(){
+    //         console.log(
+    //             $('.heading').text()
+    //         )
+    //     })
+    // })
+
+    // $(function(){
+    //     $('#btn').click(function(){
+    //         console.log(
+    //             $('.heading').html()
+    //         )
+    //     })
+    // })
+
+
+//     $(function () {
+//         $('#btn').click(function () {
+//             console.log(
+//                 $('.heading').html()
+//             )
+//         })
+//     })
+
+// }
+
+// $(function(){
+//     $('#btn').click(function(){
+//         console.log(
+//             $('.heading').html('<span>this is heading tag</span>')
+//         )
+//     })
+// })
+
+// $(function(){
+//     $('#btn').click(function(){
+//         console.log(
+//             $('.heading').text('<span>this is heading tag</span>')
+//         )
+//     })
+// })
+
+// $(function(){
+//     $('#btn').click(function(){
+//         console.log(
+//             $('#fName').val('<span>this is heading tag</span>')
+//         )
+//     })
+// })
