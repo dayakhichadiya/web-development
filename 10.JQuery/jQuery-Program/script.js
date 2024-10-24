90ed5315a85a0c03163fb2047de3d7b53252bc68
DayaKhichadiya <khichadiyadayam@gmail.com>
2024-10-24 17:58:30 +0530

    24-10-24

--- 10.JQuery/jQuery-Program/script.js
+++ 10.JQuery/jQuery-Program/script.js
@@ -32,33 +32,140 @@
 
 // 4. FadeIn your boxes
 
-$(function(){
-    $('#btn').click(function(){
-        $('#div-1').fadeIn('slow')
-        $('#div-2').fadeIn('fast')
-        $('#div-3').fadeIn('2000')
-        $('#div-4').fadeIn('6000')
-    })
-})
+// $(function(){
+//     $('#btn').click(function(){
+//         $('#div-1').fadeIn('slow')
+//         $('#div-2').fadeIn('fast')
+//         $('#div-3').fadeIn('2000')
+//         $('#div-4').fadeIn('6000')
+//     })
+// })
 
 // 5. Clickable button after checking the box
 
+// $(document).click(function () {
+//     $('#checkbox').change(function () {
+//         if ($(this).is(':checked')) {
+//             $('#btn').prop('disabled', false);
+//         } else {
+//             $('#btn').prop('disabled', true);
+//         }
+//     });
+// });
+
 // 6 . Letting Users to print some documents
+
+{
+    // $(function(){
+    //     $('#btn').click(function(){
+    //         window.print()
+    //     })
+    // })
+}
 
 // 7 . Don't let username to be too long
 
+{
+    // $(document).click(function() {
+    //         const inputlength = 5; 
+          
+    //         $('#username').on('input', function() {
+    //           const username = $(this).val();
+          
+    //           if (username.length > inputlength) {
+    //             $('#error').show(); 
+    //           } else {
+    //             $('#error').hide(); 
+    //           }
+    //         });
+    //       });
+}
 // 8 . Forget to bold words_
+
+{
+    // $(function () {
+    //     $('p').click(function () {
+    //         $(this).css({
+    //             "font-weight": "bold",
+    //             "font-size": "18px",
+    //         })
+    //     })
+    // })
+}
+
 // 9 . Adding new division
 
+{
+    // $(function(){
+    //     $('#btn').click(function(){
+    //         $('body').append('New DIV')
+    //     })
+    // })
+}
+
 // 10. Select values from a JSON object using jQuery
+
+{
+    $(function () {
+
+        var JSON = [
+            { "name": "gujarat", "text": "gujarat" },
+            { "name": "surat", "text": "surat" },
+            { "name": "Mumbai", "text": "Mumbai" },
+            { "name": "Delhi", "text": "Delhi" }
+        ]
+        $('#btn').click(function () {
+            var select = $("<select> </select>")
+            .attr("name", "cities");
+
+            $.each(json, function(index, json){
+                select.append($("<option></option>")
+                .attr("value", json.name).text(json.text));
+
+            })
+
+            $("#data").html(select)
+        })
+    })
+
+    // $(document).click(function() {
+    //         const user = {
+    //           "name": "rohan shah",
+    //           "age": 24,
+    //           "email": "rohan@gmail.com"
+    //         };
+          
+    //         $('#data').click(function() {
+    //           $('#name').text("Name: " + user.name);
+    //           $('#age').text("Age: " + user.age);
+    //           $('#email').text("Email: " + user.email);
+    //         });
+    //       });
+}
 // 11. Add element within ul list
 
 {
-    $(function(){
-        $('#btn').click(function(){
-            $('menu').append('<li><a href="#">New Item</a></li>')
-        })
-    })
+    // $(function(){
+    //     $('#btn').click(function(){
+    //         $('ul').append('<li>New Item</li>')
+    //     })
+    // })
 }
 // 12. Know what user puts into textbox
+
+{
+    // $(function(){
+    //     $('#Get').click(function(){
+    //         $('#msg').html($('input').val())
+    //     })
+    // })
+}
 // 13. Change input value
+
+{
+    // $(function () {
+    //     $('#btn').click(function () {
+    //         $('#input').val("Have a nice day")
+    //     })
+    // })
+}
