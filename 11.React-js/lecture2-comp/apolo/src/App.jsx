15cc7669d1ad07c28c11f3e7a89aa7063ab4aaaa
dayakhichadiya <khichadiyadayam@gmail.com>
2024-11-27 16:57:47 +0530

    27-11

---
+++ 11.React-js/lecture2-comp/apolo/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './component/Card'
+
+function App() {
+  <>
+  <h1>This is heading tag</h1>
+  <Card/>
+  </>
+}
+
+export default App
---
+++ 11.React-js/lecture2-comp/apolo/src/component/Card.jsx
@@ -0,0 +1,10 @@
+
+const Card = () => {
+    return (
+        <>
+        <h1>This is a updated card</h1>
+        </>
+    )
+}
+
+export default Card
