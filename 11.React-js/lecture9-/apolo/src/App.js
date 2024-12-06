cae01126830f5dd87333e43f8416610a73594e86
dayakhichadiya <khichadiyadayam@gmail.com>
2024-12-06 17:58:18 +0530

    6-12

---
+++ 11.React-js/lecture9-/apolo/src/App.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+
+export default function ActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWVxiyGxrxC0LA8DXaLGzXirEklXasImdDvRbt7Vmi0tXrW40q5RM2AHH87TUKXU5WmKg&usqp=CAU"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
